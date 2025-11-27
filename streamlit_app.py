6c3524b3dadf7e510e74fd4e409302814786886b
bendahoukawther061-arch <bendahoukawther061@gmail.com>
2025-11-27 11:56:04 +0100

    Refactor authentication and update password

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as sti
 import json
 import os
 from datetime import datetime
@@ -11,13 +11,13 @@ import base64
 # ---------------------------
 st.set_page_config(page_title="Mini Cones", page_icon="🍦")
 
-PASSWORD = "bendahou mehdi"
+PASSWORD = "mehdi123"
 
 if "auth" not in st.session_state:
     st.session_state.auth = False
 
 if not st.session_state.auth:
-    st.title("🔒 Connexion")
+    st.title("Mini Cones")
     pwd = st.text_input("Mot de passe", type="password")
     if st.button("Valider"):
         if pwd.lower() == PASSWORD:
@@ -276,3 +276,4 @@ elif page == "Historique":
             pdf_bytes = f.read()
             b64 = base64.b64encode(pdf_bytes).decode()
         st.markdown(f'<a download="{file_path}" href="data:application/pdf;base64,{b64}">📥 Télécharger PDF tableau produits</a>', unsafe_allow_html=True)
+
